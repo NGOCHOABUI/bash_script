993be09aff7aacb67bd89a798677512aebd0637a
NGOCHOABUI <buingochoa1987@gmail.com>
2020-07-31 11:23:53 +0700

    update script

--- project_setup.sh
+++ project_setup.sh
@@ -1,8 +1,11 @@
-cd  ~/CoderSchool
-mkdir project
-cd project
-mkdir data scripts notebooks data/raw data/train data/test data/validation
-mkdir app app/blueprints app/static app/templates app/blueprints/home_page
-touch app/app.py app/.gitignore app/blueprints/__init__.py app/blueprints/home_page/__init__.py app/blueprints/home_page/blueprint.py
-touch app/static/styles.css app/static/index.js  app/templates/base.html app/templates/homehtml
+mkdir data data/raw data/train data/test data/validation
+mkdir app app/blueprints app/blueprints/home_page
+touch app/blueprints/__init__.py app/blueprints/home_page/__init__.py app/blueprints/home_page/blueprint.py
+mkdir app/static
+touch app/static/style.css app/static/index.js
+mkdir app/templates
+touch app/templates/base.html app/templates/home.html
+touch app/app.py app/.gitignore
+mkdir scripts
 touch scripts/preprocess.py scripts/model.py scripts/train.py
+mkdir notebooks 
